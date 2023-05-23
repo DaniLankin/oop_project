3c00e3c4f2a621cb6dfdfe60ab434d6bfc9d38c2
danik <lankin1107@gmail.com>
2023-05-23 12:46:32 +0300

    fggsf

---
+++ finalproject/Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace finalproject
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.lb = new System.Windows.Forms.Label();
+            this.square = new System.Windows.Forms.Label();
+            this.labelpoint = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackgroundImage = global::finalproject.Properties.Resources._2023_05_22_;
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.button1.Location = new System.Drawing.Point(978, 337);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(85, 19);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "creating";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.button1_MouseClick);
+            // 
+            // lb
+            // 
+            this.lb.AutoSize = true;
+            this.lb.Location = new System.Drawing.Point(1024, 314);
+            this.lb.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lb.Name = "lb";
+            this.lb.Size = new System.Drawing.Size(41, 13);
+            this.lb.TabIndex = 1;
+            this.lb.Text = "triangle";
+            this.lb.Click += new System.EventHandler(this.triangle_Click);
+            // 
+            // square
+            // 
+            this.square.AutoSize = true;
+            this.square.Location = new System.Drawing.Point(983, 314);
+            this.square.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.square.Name = "square";
+            this.square.Size = new System.Drawing.Size(39, 13);
+            this.square.TabIndex = 2;
+            this.square.Text = "square";
+            this.square.Click += new System.EventHandler(this.square_Click);
+            // 
+            // labelpoint
+            // 
+            this.labelpoint.AutoSize = true;
+            this.labelpoint.Location = new System.Drawing.Point(1024, 65);
+            this.labelpoint.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.labelpoint.Name = "labelpoint";
+            this.labelpoint.Size = new System.Drawing.Size(26, 13);
+            this.labelpoint.TabIndex = 3;
+            this.labelpoint.Text = "(x,y)";
+            this.labelpoint.Click += new System.EventHandler(this.labelpoint_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::finalproject.Properties.Resources._2023_05_22_;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ClientSize = new System.Drawing.Size(1094, 425);
+            this.Controls.Add(this.labelpoint);
+            this.Controls.Add(this.square);
+            this.Controls.Add(this.lb);
+            this.Controls.Add(this.button1);
+            this.DoubleBuffered = true;
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.MainForm_Paint);
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.MainForm_MouseClick);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lb;
+        private System.Windows.Forms.Label square;
+        private System.Windows.Forms.Label labelpoint;
+    }
+}
+
---
+++ finalproject/Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace finalproject
+{
+    public partial class Form1 : Form
+    { 
+        int formtype,pointcounter;
+        private List<Point> points;
+        //Point[] pointarry;
+        public Form1()
+        {
+            InitializeComponent();
+            points = new List<Point>();
+        }
+
+        private void button1_MouseClick(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void square_Click(object sender, EventArgs e)
+        {
+            formtype = 4;
+            //pointarry = new Point[4];
+            MessageBox.Show("please click 4 points:");
+        }
+
+        private void triangle_Click(object sender, EventArgs e)
+        {
+            formtype = 3;
+            //pointarry = new Point[3]; 
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            this.MouseClick += MainForm_MouseClick;
+            this.Paint += MainForm_Paint;
+        }
+
+        private void MainForm_Paint(object sender, PaintEventArgs e)
+        {
+
+            if (false)
+            {
+                // Draw lines between the points
+                using (Pen pen = new Pen(Color.Black))
+                {
+                    for (int i = 1; i < points.Count; i++)
+                    {
+                        e.Graphics.DrawLine(pen, points[i - 1], points[i]);
+                    }
+                }
+            }
+
+            // Draw points
+            foreach (Point point in points)
+            {
+                e.Graphics.FillEllipse(Brushes.Red, point.X - 2, point.Y - 2, 5, 5);
+            }
+        }
+
+        private void MainForm_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (pointcounter <= formtype)
+            {
+                if (pointcounter == 0)
+                {
+                    points.Add(e.Location);
+                    pointcounter++;
+                }
+
+                if (pointcounter > 1 && points.Last() != e.Location)
+                {
+                    points.Add(e.Location);
+                    pointcounter++;
+                }
+               
+                
+            }
+            
+            this.Invalidate(); // Invalidate the form to trigger the Paint event
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+
+        {
+            labelpoint.Text = "" + (e.Location.X + 119) + ","+ (e.Location.Y + 410);
+        }
+
+        private void labelpoint_Click(object sender, EventArgs e)
+        {
+        }
+
+        /*
+        private void formclick(object sender, MouseEventArgs e)
+        {
+            if (pointcounter < formtype)
+            {
+                points[pointcounter] = e.Location;
+                pointcounter++;
+            }
+
+            if (pointcounter == formtype)
+            {
+                // All points have been picked
+                int a = 0;
+            }
+        }*/
+    }
+}
+
